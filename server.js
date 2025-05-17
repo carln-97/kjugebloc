395bb9e847d06ea67599d6983253cf60478ee156
carln-97 <94535340+carln-97@users.noreply.github.com>
2025-05-17 21:13:03 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 const http = require('http');
 const fs = require('fs');
 const path = require('path');
-
 const PORT = process.env.PORT || 8000;
 
 http.createServer((req, res) => {
-  const reqPath = req.url.split('?')[0]; // Remove query string
-  let filePath = '.' + (reqPath === '/' ? '/index.html' : reqPath);
-  const ext = path.extname(filePath);
+  // Strip off any query strings (?...) and fragments (#...)
+  const cleanPath = req.url.split('?')[0].split('#')[0];
+  const filePath = '.' + (cleanPath === '/' ? '/index.html' : cleanPath);
+  const ext = path.extname(filePath).toLowerCase();
 
   let contentType = 'text/html';
   switch (ext) {
@@ -18,11 +18,12 @@ http.createServer((req, res) => {
     case '.jpg':
     case '.jpeg': contentType = 'image/jpeg'; break;
     case '.svg': contentType = 'image/svg+xml'; break;
+    default: contentType = 'text/html'; break;
   }
 
   fs.readFile(filePath, (err, content) => {
     if (err) {
-      res.writeHead(404);
+      res.writeHead(404, { 'Content-Type': 'text/plain' });
       res.end('Not Found');
     } else {
       res.writeHead(200, { 'Content-Type': contentType });
