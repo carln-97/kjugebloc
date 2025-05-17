a8b14c8230e5c9bd3375d6bbc55a352ead6c6f51
carln-97 <94535340+carln-97@users.noreply.github.com>
2025-05-17 21:25:46 +0200

    Update server.js

--- server.js
+++ server.js
@@ -2,9 +2,12 @@ const http = require('http');
 const fs = require('fs');
 const path = require('path');
 
+const PORT = process.env.PORT || 8000;
+
 http.createServer((req, res) => {
-  let filePath = '.' + (req.url === '/' ? '/index.html' : req.url);
-  const ext = path.extname(filePath);
+  const cleanPath = req.url.split('?')[0].split('#')[0];  // strips query & hash
+  const filePath = '.' + (cleanPath === '/' ? '/index.html' : cleanPath);
+  const ext = path.extname(filePath).toLowerCase();
 
   let contentType = 'text/html';
   switch (ext) {
@@ -15,18 +18,18 @@ http.createServer((req, res) => {
     case '.jpg':
     case '.jpeg': contentType = 'image/jpeg'; break;
     case '.svg': contentType = 'image/svg+xml'; break;
+    default: contentType = 'text/html'; break;
   }
 
-  // Stream file if exists
   fs.readFile(filePath, (err, content) => {
     if (err) {
-      res.writeHead(404);
+      res.writeHead(404, { 'Content-Type': 'text/plain' });
       res.end('Not Found');
     } else {
       res.writeHead(200, { 'Content-Type': contentType });
       res.end(content);
     }
   });
-}).listen(8000);
-
-console.log('Server running at http://localhost:8000');
+}).listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
